c2d2e466dfa75a8610babc014598dac3aeaea396
shu-illy <sh.illy.9117@gmail.com>
2022-03-01 08:57:46 +0900

    lesson41

--- src/pages/task-page.jsx
+++ src/pages/task-page.jsx
@@ -4,6 +4,8 @@ import Link from "next/link";
 import { getAllTasksData } from "src/lib/tasks";
 import Task from "src/components/Task";
 import useSWR from "swr";
+import StateContextProvider from "src/context/StateContext";
+import TaskForm from "src/components/TaskForm";
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 const apiUrl = `${process.env.NEXT_PUBLIC_RESTAPI_URL}api/list-task/`;
@@ -18,33 +20,36 @@ export default function TaskPage({ staticFilteredTasks }) {
     mutate();
   }, []);
   return (
-    <Layout title="Task page">
-      <ul>
-        {filteredTasks &&
-          filteredTasks.map((task) => (
-            <Task key={task.id} task={task} taskDeleted={mutate} />
-          ))}
-      </ul>
-      <Link href="/main-page" passHref>
-        <div className="flex cursor-pointer mt-12">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 mr-4"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth={2}
-              d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
-            />
-          </svg>
-          <span>Back to main page</span>
-        </div>
-      </Link>
-    </Layout>
+    <StateContextProvider>
+      <Layout title="Task page">
+        <TaskForm taskCreated={mutate} />
+        <ul>
+          {filteredTasks &&
+            filteredTasks.map((task) => (
+              <Task key={task.id} task={task} taskDeleted={mutate} />
+            ))}
+        </ul>
+        <Link href="/main-page" passHref>
+          <div className="flex cursor-pointer mt-12">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-6 w-6 mr-4"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth={2}
+                d="M11 19l-7-7 7-7m8 14l-7-7 7-7"
+              />
+            </svg>
+            <span>Back to main page</span>
+          </div>
+        </Link>
+      </Layout>
+    </StateContextProvider>
   );
 }
 export async function getStaticProps() {
